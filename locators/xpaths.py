6cf7262661b43788588ca6b6a125dd656075074b
ThallapalliVenugopi <thallapalli2000@gmail.com>
2026-02-27 11:46:54 +0530

    updated framework

---
+++ locators/xpaths.py
@@ -0,0 +1,4 @@
+user_name_input_xpath = "user-name"
+password_input_xpath = "password"
+login_button_xpath = "//input[@ID='login-button']"
+burger_menu_button_xpath = 'react-burger-menu-btn'
---
+++ tests/login_to_oragne_hrm_test.py
@@ -0,0 +1,9 @@
+from orng_hrm_lib import login
+
+def login_test():
+    login.login_to_orange_hrm()
+
+
+
+if __name__ == "__main__":
+    login_test()
