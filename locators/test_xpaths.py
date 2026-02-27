aecd725653a98aa5029a8e24b47c626c8c4fe155
Venu Gopi <thallapalli2000@gmail.com>
2026-02-27 15:12:42 +0530

    adding new files

---
+++ locators/test_xpaths.py
@@ -0,0 +1,3 @@
+user_name_xpath = "username"
+password_xpath = "password"
+click_button = '//button[@type = "submit"]'
---
+++ url/test_url.py
@@ -0,0 +1,1 @@
+orange_hrm_url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
